c56d1df15b89865949d41e4d969140e44a2ab96d
Oleg Zagorevsky <pboul-ooo@yandex.ru>
2022-06-06 15:59:52 +0300

    feat:LoginUser

---
+++ src/components/LoginUser.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <h4>Авторизация</h4>
+    <form>
+      <label for="email">E-Mail</label>
+      <div>
+        <input id="email" type="email" v-model="email" required autofocus>
+      </div>
+      <div>
+        <label for="password">Пароль</label>
+        <div>
+          <input id="password" type="password" v-model="password" required>
+        </div>
+      </div>
+      <div>
+        <button type="submit" @click="handleSubmit">
+          Войти
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: ""
+    }
+  },
+  methods: {
+    handleSubmit(e) {
+      e.preventDefault()
+      if (this.password.length > 0) {
+        this.$http.post('//localhost:3000/login', {
+          email: this.email,
+          password: this.password
+        })
+          .then(response => {
+            localStorage.setItem('user', JSON.stringify(response.data.user))
+            localStorage.setItem('jwt', response.data.token)
+
+            if (localStorage.getItem('jwt') != null) {
+              this.$emit('loggedIn')
+              if (this.$route.params.nextUrl != null) {
+                this.$router.push(this.$route.params.nextUrl)
+              }
+              else {
+                if (is_admin == 1) {
+                  this.$router.push('admin')
+                }
+                else {
+                  this.$router.push('dashboard')
+                }
+              }
+            }
+          })
+          .catch(function (error) {
+            console.error(error.response);
+          });
+      }
+    }
+  }
+}
+</script>
--- src/router/router.js
+++ src/router/router.js
@@ -14,7 +14,7 @@ const routes = [
   },
   {
     path: '/register',
-    name: 'register',
+    name: 'register-user',
     component: RegisterUser,
     meta: {
       guest: true
